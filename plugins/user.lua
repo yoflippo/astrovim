4cb5323c59c23ba803ffdae40968af214cc8ab56
yof <yof@pop-os.localdomain>
2023-05-10 12:10:56 +0200

    added hop and better-escape jj

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,14 +1,24 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  "lervag/vimtex",
-   lazy=false
+ -- You can also add new plugins here as well:
+ -- Add plugins, the lazy syntax
+ -- "andweeb/presence.nvim",
+ -- {
+ --   "ray-x/lsp_signature.nvim",
+ --   event = "BufRead",
+ --   config = function()
+ --     require("lsp_signature").setup()
+ --   end,
+ -- },
+ { "lervag/vimtex",                lazy = false },
+ { "max397574/better-escape.nvim", event = "InsertCharPre", opts = { timeout = 300, mapping = { "jj" } } },
+ {
+  "phaazon/hop.nvim",
+  branch = 'v2',
+  lazy = false,
+  config = function()
+   -- you can configure Hop the way you like here; see :h hop-config
+   require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+  end
+ },
+
 }
