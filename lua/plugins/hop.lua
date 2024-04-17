c22a664cbe11528ebd2ff8e99f7161d5a1e2f431
M. Schrauwen <mark@lionite.nl>
2024-04-17 22:18:24 +0200

    ASTROVIM v4.1

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,15 @@
+ -- return{
+ --      "phaazon/hop.nvim",
+ --      branch = 'v2',
+ --      lazy = false,
+ --      config = function()
+ --       -- you can configure Hop the way you like here; see :h hop-config
+ --       require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+ --      end
+ -- }
+
+return{
+    'smoka7/hop.nvim',
+    version = "*",
+    opts = {},
+}
