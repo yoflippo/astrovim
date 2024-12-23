bd6e1807703b3c2ce09b93b06aa808354fccea39
M. Schrauwen <mark@lionite.nl>
2024-12-23 16:47:32 +0100

    Update ouroboros

--- lua/plugins/ouroboros.lua
+++ lua/plugins/ouroboros.lua
@@ -1,6 +1,18 @@
-return{
-    'jakemason/ouroboros',
+return {
+    {
+        'jakemason/ouroboros',
         version = "*",
-        opts = {},
-        requires = { {'nvim-lua/plenary.nvim'} },
+        requires = { { 'nvim-lua/plenary.nvim' } },
+        opts = {
+            extension_preferences_table = {
+                c = { h = 2, hpp = 1 },
+                h = { c = 2, cc = 1 },
+                cpp = { hpp = 2, h = 1 },
+                cc = { hpp = 2, h = 1 },
+            },
+            -- If this is true and the matching file is already open in a pane,
+            -- we'll switch to that pane instead of opening it in the current buffer
+            switch_to_open_pane_if_possible = false,
+        },
+    },
 }
